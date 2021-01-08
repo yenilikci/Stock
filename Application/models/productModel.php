eb452fd68b56d4ddfb464659b2204d017a4de7a2
Melih Çelik <57464067+yenilikci@users.noreply.github.com>
2021-01-08 22:20:48 +0300

    Ürün Listeleme İşlemi ~ index, listview

--- Application/models/productModel.php
+++ Application/models/productModel.php
@@ -8,19 +8,4 @@ class productModel extends model
         $query->execute();
         return $query->fetchAll(PDO::FETCH_ASSOC);
     }
-
-    public function create($ad,$kategoriId,$ozellikler)
-    {
-        $query = $this->db->prepare("insert into urunler(ad,kategoriid,ozellikler,tarih) values(?,?,?,?)");
-        $date = date('Y-m-d');
-        $insert = $query->execute(array($ad,$kategoriId,$ozellikler,$date));
-        if ($insert)
-        {
-            return true;
-        }
-        else {
-            return false;
-        }   
-    }
-
 }
